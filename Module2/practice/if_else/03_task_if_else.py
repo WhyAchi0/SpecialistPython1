580fda58764b38ad42b93cf8233c98456aeb0c67
WhyAchi0 <96467919+WhyAchi0@users.noreply.github.com>
2021-12-21 12:24:59 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -3,3 +3,11 @@
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
 # TODO: your code here
+
+a = int(input("First side of triangle ="))
+b = int(input("Second side of triangle ="))
+c = int(input("Third side of triangle ="))
+if a == b or a == c or b==c:
+    print ("Yes, the triangle is isosceles")
+else:
+    print ("No, the triangle isn't isosceles")
